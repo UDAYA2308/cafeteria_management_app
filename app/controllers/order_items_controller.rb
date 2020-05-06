59fd539109adbd40f94f9f69162817a7a1eef519
udaya2308 <udaya14apr2000@gmail.com>
2020-05-06 21:55:40 +0530

    updated order_item details page

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -6,6 +6,7 @@ class OrderItemsController < ApplicationController
   def index
     @order_items = OrderItem.get_items(params[:order_id])
     @amount = OrderItem.get_total(params[:order_id])
+    @user_data = Order.get_details(params[:order_id])
   end
 
   # GET /order_items/1
--- app/models/order.rb
+++ app/models/order.rb
@@ -12,4 +12,12 @@ class Order < ApplicationRecord
     end
     Order.where("date > ? and date < ? ", from, to)
   end
+
+  def self.get_details(order_id)
+    user_data = {}
+    user_data["name"] = Order.find(order_id).user.first_name
+    user_data["date"] = Order.find(order_id).date.to_date
+    user_data["email"] = Order.find(order_id).user.email
+    user_data
+  end
 end
